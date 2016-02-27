32a19e30f7d848171b88281f9e93e132d34f6d9f
Renée Cruz <reneebasscruz@gmail.com>
2016-02-27 13:15:02 -0500

    Added 'Status' class to Post Models for 'Draft' and 'Published' blog posts.

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    STATUSES = (
+        ('D', 'Draft'),
+        ('P', 'Published')    
+    )
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -10,10 +14,13 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
-
+    status = models.CharField(max_length=1, choices=STATUSES, default='P')
+    
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+
+
